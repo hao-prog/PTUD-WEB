d2de673e2f87c6ffe3a2b1e984afdeefcaa3c07b
NVH <nvhoan911@gmail.com>
2021-12-27 12:06:53 +0700

    Search and delete score

---
+++ searchScore.php
@@ -0,0 +1,5 @@
+<?php
+    require_once 'app/common/connectionPDO.php';
+    include 'app/model/quanlydiem.php';
+    include 'app/controller/quanlydiemController.php';
+    require_once('app/view/quanlydiem/searchScoreView.php');
