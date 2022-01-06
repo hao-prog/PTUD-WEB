b31094bbda0878a2156ac8dcf323915f246f9551
hoangvanhai <hai10hoangvan@gmail.com>
2022-01-07 01:01:08 +0700

    Add files via upload

---
+++ Search_teacher/app/common/define.php
@@ -0,0 +1,17 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "qldsv");
+define('SPECIALIZED_ARR', [
+    '001' => 'Khoa học máy tính',
+    '002' => 'Khoa học dữ liệu',
+    '003' => 'Hải dương học'
+]);
+define('DEGREE_ARR', [
+    '001' => 'Cử nhân',
+    '002' => 'Thạc sĩ',
+    '003' => 'Tiến sĩ',
+    '004' => 'Phó giáo sư',
+    '005' => 'Giáo sư'
+]);
