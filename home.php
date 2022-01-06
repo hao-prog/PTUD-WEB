000dbd7a7f77dff2c8f7ae19ef9c0eafb4f04b18
Giang0606 <dieuly0606@gmail.com>
2022-01-06 23:42:13 +0700

    modified access

--- home.php
+++ home.php
@@ -5,7 +5,8 @@
 		<link rel="stylesheet" type="text/css" href="web/css/home.css?v=<?php echo time(); ?>">
         <?php   
             header('Content-Type: text/html; charset=utf-8');
-            include 'app/controller/common.php';
+            session_start();
+            include 'access.php';
             expire("");
         ?>
     </head>
