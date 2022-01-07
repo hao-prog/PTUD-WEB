1579b19cc3d4f783ed98f127d7c0aa3e0c271f38
NVH <nvhoan911@gmail.com>
2022-01-07 10:17:56 +0700

    no message

--- add_score.php
+++ add_score.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 header('Content-Type: text/html; charset=utf-8');
 session_start();
 error_reporting(0);
@@ -9,8 +8,3 @@ expire("");
 require_once 'app/common/connectionPDO.php';
 include 'app/model/quanlydiem.php';
 include 'app/controller/scoresController/scores_add.php';
-=======
-require_once 'app/common/connectionPDO.php';
-include 'app/model/quanlydiem.php';
-include 'app/controller/scoresController/scores_add.php';
->>>>>>> 05c842e41de016a75c71368028c61e25ad41f279
