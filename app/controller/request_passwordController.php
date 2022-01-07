62a76abb905bcceb3fbaca3fabfc242e14ff8ab4
Giang0606 <68329272+Giang0606@users.noreply.github.com>
2022-01-07 14:05:04 +0700

    update file depend

--- app/controller/request_passwordController.php
+++ app/controller/request_passwordController.php
@@ -1,6 +1,6 @@
 <?php 
     
-    require "app/model/admin.php";
+//     require "app/model/admin.php";
     $login_idErr = "";
     $login_id = "";
     $reset_password_token = microtime(true);
@@ -26,7 +26,7 @@
         }
     }
 
-    require_once('app/view/request_passwordView.php');
+//     require_once('app/view/request_passwordView.php');
 
 
                     
