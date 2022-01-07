7a49d833be6c6f4772e4eed98823883eb7d03ee0
Giang0606 <68329272+Giang0606@users.noreply.github.com>
2022-01-07 14:01:29 +0700

    update file depend

--- app/view/request_passwordView.php
+++ app/view/request_passwordView.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/PTUD-WEB-De-tai-3-Quan-ly-diem-sinh-vien/web/css/request_pass.css">
     <title>Request Password</title>
+     <?php
+    require_once '../common/connectionPDO.php';
+    include '../model/admin.php';
+    include '../controller/request_passwordController.php'; 
+    ?>
 </head>
 <body>
     <div class="request">
