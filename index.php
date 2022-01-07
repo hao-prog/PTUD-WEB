45cf90c4d2815d59fb6c814b2c1a1fc4d7652e01
Giang0606 <dieuly0606@gmail.com>
2022-01-07 16:40:32 +0700

    change default link

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
     // include_once 'app/controller/common.php';
     include_once 'app/controller/reset_passwordController.php';
     // include_once 'app/controller/request_passwordController.php';
-
+    header('Location: login.php');
 
 ?>
