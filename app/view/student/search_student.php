3d5d0b772aa21f22a3f9fdcd5c918d985122f774
Giang0606 <dieuly0606@gmail.com>
2022-01-08 09:42:18 +0700

    add access

--- app/view/student/search_student.php
+++ app/view/student/search_student.php
@@ -8,6 +8,11 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <?php
+    session_start();
+    include '../../../access.php';
+    expire("../../../");
+    ?>
 </head>
 <body>
 <div class="content">
