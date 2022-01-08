8cb12e4cd16f1ac5de43042f3c3a19272b8cde22
NVH <nvhoan911@gmail.com>
2022-01-09 00:26:51 +0700

    no message

--- app/view/subject/subject_add_input_view.php
+++ app/view/subject/subject_add_input_view.php
@@ -14,7 +14,7 @@ expire('../../../');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../web/css/subject/addSubject.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <title>Thêm phòng học</title>
+    <title>Thêm môn học</title>
 </head>
 <body>
     <div class="content container">
--- app/view/subject/subject_edit_input_view.php
+++ app/view/subject/subject_edit_input_view.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../web/css/subject/editSubject.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <title>Sửa môns học</title>
+    <title>Sửa môn học</title>
 </head>
 
 <body>
