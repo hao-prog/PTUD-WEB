94bfe06c4826dab72604603c5d63b7f3b3137e12
hao-prog <hao02532000@gmail.com>
2022-01-08 23:06:20 +0700

    Thêm data cho bảng scores, students, subjects

--- app/common/qldsv.sql
+++ app/common/qldsv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th1 07, 2022 lúc 11:10 AM
--- Phiên bản máy phục vụ: 10.4.13-MariaDB
--- Phiên bản PHP: 7.4.7
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2022 at 05:03 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `qldsv`
+-- Database: `qldsv`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `admins`
+-- Table structure for table `admins`
 --
 
 CREATE TABLE `admins` (
@@ -38,7 +38,7 @@ CREATE TABLE `admins` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Đang đổ dữ liệu cho bảng `admins`
+-- Dumping data for table `admins`
 --
 
 INSERT INTO `admins` (`id`, `login_id`, `password`, `actived_flag`, `reset_password_token`, `updated`, `created`) VALUES
@@ -49,7 +49,7 @@ INSERT INTO `admins` (`id`, `login_id`, `password`, `actived_flag`, `reset_passw
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `scores`
+-- Table structure for table `scores`
 --
 
 CREATE TABLE `scores` (
@@ -63,10 +63,20 @@ CREATE TABLE `scores` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `scores`
+--
+
+INSERT INTO `scores` (`id`, `student_id`, `teacher_id`, `subject_id`, `score`, `description`, `updated`, `created`) VALUES
+(1, 4, 1, 2, 6, 'có tiến bộ', '2022-01-08 23:02:02', '2022-01-08 23:02:02'),
+(2, 3, 3, 4, 6, 'khá hơn so với lần trước!', '2022-01-08 23:02:48', '2022-01-08 23:02:48'),
+(3, 1, 4, 7, 10, 'Quá tuyệt vời, quá nice sừ!', '2022-01-08 23:03:21', '2022-01-08 23:03:21'),
+(4, 2, 4, 8, 9, 'Cần cố gắng hơn chút nữa!', '2022-01-08 23:03:42', '2022-01-08 23:03:42');
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `students`
+-- Table structure for table `students`
 --
 
 CREATE TABLE `students` (
@@ -78,10 +88,20 @@ CREATE TABLE `students` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `avatar`, `description`, `updated`, `created`) VALUES
+(1, 'Trần Đức Bo', 'GGMeet_Avatar_1641657555.png', 'singer', '2022-01-08 22:59:17', '2022-01-08 22:59:17'),
+(2, 'Lê Tùng Vân', 'GGMeet_Avatar_2_1641657637.png', 'fjgfjfkgfjgkfg', '2022-01-08 23:00:38', '2022-01-08 23:00:38'),
+(3, 'Ngô Bá Khá', 'GGMeet_Avatar_3_1641657655.png', 'fjgkfgjk', '2022-01-08 23:00:57', '2022-01-08 23:00:57'),
+(4, 'Cô Ba Vàng Ngọc', 'GGMeet_Avatar_4_1641657686.png', 'jfgkfgjkfglkllkkl', '2022-01-08 23:01:27', '2022-01-08 23:01:27');
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `subjects`
+-- Table structure for table `subjects`
 --
 
 CREATE TABLE `subjects` (
@@ -94,10 +114,24 @@ CREATE TABLE `subjects` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`, `avatar`, `description`, `school_year`, `updated`, `created`) VALUES
+(1, 'OOP', NULL, 'Lập trình hướng đối tượng', 'Năm 2', '2022-01-07 20:51:14', '2022-01-07 20:51:14'),
+(2, 'Lập trình Python', NULL, 'Môn tự chọn', 'Năm 3', '2022-01-07 20:52:02', '2022-01-07 20:52:02'),
+(3, 'Phát triển ứng dụng web', NULL, 'PHP', 'Năm 4', '2022-01-07 20:52:02', '2022-01-07 20:52:02'),
+(4, 'Tin học văn phòng', NULL, 'Word, Excel', 'Năm 1', '2022-01-08 14:05:38', '2022-01-08 14:05:38'),
+(5, 'Lập trình C++', NULL, 'Môn tự chọn', 'Năm 3', '2022-01-08 14:05:38', '2022-01-08 14:05:38'),
+(6, 'Cấu trúc dữ liệu và giải thuật', NULL, 'Môn chuyên ngành', 'Năm 2', '2022-01-08 14:09:33', '2022-01-08 14:09:33'),
+(7, 'Phân tích hệ thống thông tin', NULL, 'Thực tập tại các công ty', 'Năm 4', '2022-01-08 14:09:33', '2022-01-08 14:09:33'),
+(8, 'Xử lý ảnh', NULL, 'Môn tự chọn', 'Năm 4', '2022-01-08 22:22:01', '2022-01-08 22:22:01');
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `teachers`
+-- Table structure for table `teachers`
 --
 
 CREATE TABLE `teachers` (
@@ -112,79 +146,66 @@ CREATE TABLE `teachers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `teachers`
+-- Dumping data for table `teachers`
 --
 
 INSERT INTO `teachers` (`id`, `name`, `avatar`, `description`, `specialized`, `degree`, `updated`, `created`) VALUES
-(1, 'Thầy giáo Rose', 'thayHuanRose.jpg', 'Thầy dạy môn Giáo dục công dân', '003', '002', '2021-12-31 13:18:50', '2021-12-30 14:01:41'),
+(1, 'Thầy Huấn Rose', 'thayHuanRose.jpg', 'dfgfdfhbvcvfggfdgfgdgfg', '002', '002', '2022-01-07 23:03:40', '2021-12-30 14:01:41'),
 (3, 'Thầy Huấn Rose', 'teacher_1.jpg', 'Đẹp trai, tài năng, tư duy đỉnh cao, tố chất lãnh đạo là những thứ mà Thầy Giáo Ba có', '001', '004', '2021-12-31 08:06:05', '2021-12-30 14:01:41'),
-(4, 'Cô Giáo Minh Thu', 'teacher_2.jpg', 'Cô dạy môn Vật lý rất hay', '002', '004', '2022-01-07 11:24:29', '2021-12-30 14:01:41');
+(4, 'Cô Giáo Trân', 'teacher_2.jpg', 'ghgghghh', '001', '002', '2022-01-08 12:45:32', '2021-12-30 14:01:41');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `admins`
---
-ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `login_id` (`login_id`);
-
---
--- Chỉ mục cho bảng `scores`
+-- Indexes for table `scores`
 --
 ALTER TABLE `scores`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `students`
+-- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `subjects`
+-- Indexes for table `subjects`
 --
 ALTER TABLE `subjects`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `teachers`
+-- Indexes for table `teachers`
 --
 ALTER TABLE `teachers`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `admins`
---
-ALTER TABLE `admins`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT cho bảng `scores`
+-- AUTO_INCREMENT for table `scores`
 --
 ALTER TABLE `scores`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT cho bảng `students`
+-- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT cho bảng `subjects`
+-- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT cho bảng `teachers`
+-- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
